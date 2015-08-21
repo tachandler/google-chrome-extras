814b5bcf9307b184c92aa222f69f2845117cb2db
Timothy Alan Chandler <timothy.a.chandler@gmail.com>
2015-08-21 00:17:41 -0700

    Removed stale shell script

--- autopkg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-_pkg="google-chrome-extras"
-_apps="contacts gmail tasks"
-for _app in $_apps; do
-  test -d  apps/$_app && cd apps/ && zip -q ../$_pkg-app-$_app.zip $_app/* && cd ..;
-  test -f $_pkg-app-$_app.zip && unzip -l $_pkg-app-$_app.zip
-done
